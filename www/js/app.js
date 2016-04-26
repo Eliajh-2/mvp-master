b1a4f35b474fd5c98633edf3da940608fa8db00f
marcus gray <abundanceisfree@gmail.com>
2016-04-26 14:51:06 -0700

    commit

--- www/js/app.js
+++ www/js/app.js
@@ -57,18 +57,18 @@ angular.module('artscan', ['ionic', 'artscan.controllers', 'artscan.services', '
   $ionicConfigProvider.backButton.previousTitleText(false);
   $ionicConfigProvider.backButton.text('');
 
-  // $ionicConfigProvider.platform.ios.tabs.style('standard'); 
+  // $ionicConfigProvider.platform.ios.tabs.style('standard');
   // $ionicConfigProvider.platform.ios.tabs.position('bottom');
   // $ionicConfigProvider.platform.android.tabs.style('standard');
   // $ionicConfigProvider.platform.android.tabs.position('standard');
 
-  // $ionicConfigProvider.platform.ios.navBar.alignTitle('center'); 
+  // $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
   // $ionicConfigProvider.platform.android.navBar.alignTitle('left');
 
   // $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
-  // $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');  
+  // $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
 
-  // $ionicConfigProvider.platform.ios.views.transition('ios'); 
+  // $ionicConfigProvider.platform.ios.views.transition('ios');
   // $ionicConfigProvider.platform.android.views.transition('android');
 })
 
